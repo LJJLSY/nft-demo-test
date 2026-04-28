6335f6497ac2bcd8ee3bfd10c4b509c9927d87cf
LJJLSY <1574090926@qq.com>
2026-04-28 11:36:27 +0800

    Delete ignition directory

--- ignition/modules/MetaERC20.ts
+++
@@ -1,8 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-import { parseUnits } from "ethers";
-const metaERC20Module = buildModule("MetaERC20Module", (m) => {
-    const metaERC20 = m.contract("MetaERC20", ["USDC", "USDC", parseUnits("1000000", 6)]);
-
-  return { metaERC20 };
-});
-export default metaERC20Module;
--- ignition/modules/MetaOracle.ts
+++
@@ -1,8 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-import { parseUnits } from "ethers";
-const metaOracleModule = buildModule("MetaOracleModule", (m) => {
-    const metaOracle = m.contract("MetaOracle", [parseUnits("3000", 8)]);
-
-  return { metaOracle };
-});
-export default metaOracleModule;
